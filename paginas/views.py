c49ba94c1b227c15ecf60d6f02f71aa7f246f705
Daniel Solano <solanomezaangel@gmail.com>
2024-05-15 22:54:27 -0700

    Documento base

--- paginas/views.py
+++ paginas/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -21,13 +23,18 @@ def signup(request):
                 user = User.objects.create_user(
                     username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('Usuario creado')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html',
                               {'form': UserCreationForm,
                                'error': 'El usuario ya existe en la base de datos'},
                               )
-    return render(request, 'signup.html',
-                  {'form': UserCreationForm,
-                   'error': 'Las contraseñas no coinciden'},
-                  )
+        return render(request, 'signup.html',
+                      {'form': UserCreationForm,
+                       'error': 'Las contraseñas no coinciden'},
+                      )
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
--- sgap/urls.py
+++ sgap/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('signup/', views.signup, name = 'signup'),
-    
+    path('tasks/', views.tasks, name = 'tasks'),    
 ]
