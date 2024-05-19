0870852449decaa9105a6b2c1ab8307d99357593
Daniel Solano <solanomezaangel@gmail.com>
2024-05-18 22:50:14 -0700

    Inicio de sesion

--- paginas/views.py
+++ paginas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
 
@@ -38,3 +38,26 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'El usuario y/o contraseña son incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
--- sgap/urls.py
+++ sgap/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('signup/', views.signup, name = 'signup'),
-    path('tasks/', views.tasks, name = 'tasks'),    
-]
+    path('tasks/', views.tasks, name = 'tasks'),
+    path('logout/', views.signout, name = 'logout'),
+    path('signin/', views.signin, name = 'signin'),
+    ]
