e52fdaf272367028ab696d887adaad1f65471575
Daniel Solano <solanomezaangel@gmail.com>
2024-05-22 10:43:59 -0700

    preparense... te voy a saludar
    detalle cita

--- paginas/models.py
+++ paginas/models.py
@@ -11,19 +11,103 @@ class Cita(models.Model):
         (CONFIRMADA, 'Confirmada'),
         (CANCELADA, 'Cancelada'),
     ]
-    titulo = models.CharField(max_length=100)
-    motivo = models.TextField(max_length=100)
+    
+    HOMBRE = 'Hombre'
+    MUJER = 'Mujer'
+    PREFIERO_NO_DECIRLO = 'Prefiero no decirlo'
+    GENERO = [
+        (HOMBRE, 'Hombre'),
+        (MUJER, 'Mujer'),
+        (PREFIERO_NO_DECIRLO, 'Prefiero no decirlo'),
+    ]
+# Create your models here.
+class Cita(models.Model):
+    PENDIENTE = 'Pendiente'
+    CONFIRMADA = 'Confirmada'
+    DECLINADA = 'Declinada'
+    ESTADOS_CITA = [
+        (PENDIENTE, 'Pendiente'),
+        (CONFIRMADA, 'Confirmada'),
+        (DECLINADA, 'Declinada'),
+    ]
+    
+    HOMBRE = 'Hombre'
+    MUJER = 'Mujer'
+    PREFIERO_NO_DECIRLO = 'Prefiero no decirlo'
+    GENERO = [
+        (HOMBRE, 'Hombre'),
+        (MUJER, 'Mujer'),
+        (PREFIERO_NO_DECIRLO, 'Prefiero no decirlo'),
+    ]
+    
+    INGENIERIA_CIVIL = 'Ingenieria Civil'
+    INGENIERIA_ELECTRONICA = 'Ingenieria en Electronica'
+    INGENIERIA_COMPUTACION = 'Ingenieria en Computacion'
+    INGENIERIA_INDUSTRIAL = 'Ingenieria Industrial'
+    INGENIERIA_NANOTECNOLOGIA = 'Ingenieria en Nanatecnologia'
+    INGENIERIA_SOFTWARE = 'Ingenieria en Software'
+    BIOINGENIERIA = 'Bioingenieria'
+    ARQUITECTURA = 'Arquitectura'
+    CARRERAS = [
+        (INGENIERIA_CIVIL,  'Ingenieria Civil'),
+        (INGENIERIA_ELECTRONICA,  'Ingenieria en Electronica'),
+        (INGENIERIA_COMPUTACION,  'Ingenieria en Computacion'),
+        (INGENIERIA_INDUSTRIAL,  'Ingenieria Industrial'),
+        (INGENIERIA_NANOTECNOLOGIA,  'Ingenieria en Nanotecnologia'),
+        (INGENIERIA_SOFTWARE,  'Ingenieria en Software y Tecnologias Emergentes'),
+        (BIOINGENIERIA,  'Bioingenieria'),
+        (ARQUITECTURA,  'Arquitectura'),
+    ]
+    
+    ACADEMICA = 'Academica'
+    PERSONAL = 'Personal'
+    VOCACIONAL = 'Vocacional'
+    BAJA = 'Baja'
+    PSICOLOGICA = 'Psicologica'
+    ASUNTOS = [
+        (ACADEMICA, 'Academica'),
+        (PERSONAL, 'Personal'),
+        (VOCACIONAL, 'Vocacional'),
+        (BAJA, 'Baja'),
+        (PSICOLOGICA, 'Psicologica'),
+    ]
+    
+    matricula = models.CharField(max_length=8)
+    ap_paterno = models.CharField(max_length=20)
+    ap_materno = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=40)
+    descripcion = models.TextField(max_length = 200)
+    semestre = models.CharField(max_length = 2)
     fecha = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sexo = models.CharField(
+        max_length = 20,
+        choices = GENERO,
+        default = PREFIERO_NO_DECIRLO,
+    )
+    
     estado = models.CharField(
         max_length=11,
         choices=ESTADOS_CITA,
         default=PENDIENTE,
     )
+    
+    carrera = models.CharField(
+        max_length=50,
+        choices = CARRERAS,
+        default = INGENIERIA_SOFTWARE
+    )
+    
+    asunto = models.CharField(
+        max_length=50,
+        choices = ASUNTOS,
+        default = ACADEMICA
+    )
+    
     solicitada = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
-        return ' ' + self.titulo + ' - ' + self.user.username
+        return ' ' + self.asunto + ' - ' + self.user.username
     
 class Horario(models.Model):
     ESTADO_CHOICES = [
