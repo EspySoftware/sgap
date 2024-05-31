1a130c29040a62ac26e8c0bb0ff9cf9f6aaf70d0
Daniel Solano <solanomezaangel@gmail.com>
2024-05-30 23:39:14 -0700

    cambio base de datos

--- sgap/settings.py
+++ sgap/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'sgap.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default = 'DATABASE_URL',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
