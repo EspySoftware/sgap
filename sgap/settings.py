34f848a81b70bb091f08755c2e8ced0dd844d6df
Daniel Solano <solanomezaangel@gmail.com>
2024-05-30 23:29:28 -0700

    Update settings.py

--- sgap/settings.py
+++ sgap/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'sgap.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = 'postgresql://postgres:postgres@localhost:5432/mysite',
+        default = 'postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
