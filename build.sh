3d2f20263c7a60dd26ce119a0c1b68da89331eb6
Daniel Solano <solanomezaangel@gmail.com>
2024-05-30 22:59:32 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
